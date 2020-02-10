1adffd0c8fa37df8b933973ec5addf719630506f
ma38su <ma38su@gmail.com>
2020-02-11 01:14:28 +0900

    add fib() test

--- test.c
+++ test.c
@@ -4,19 +4,63 @@ int a;
 int b;
 char c;
 
-char* s = "Hello\n";
-char t[] = "World\n";
+char* s = "Hello";
+char t[] = "World";
+
+int sum() {
+  int sum = 0;
+  int i;
+  for (i = 0; i < 10; i = i + 1) {
+    sum = sum + i;
+  } 
+  return sum;
+}
+
+void test1() {
+  int v = sum();
+  if (v == 45) {
+    printf("OK\n");
+  } else {
+    printf("NG: actual is %d, expected is %d.\n", v, 45);
+  }
+}
+
+void test2() {
+  printf("printf(char*)  %s\n", s);
+  printf("printf(char[]) %s\n", t);
+}
+
+
+int fib(int a) {                                                                                                                                                               
+  if (a <= 0) {
+    return 0;
+  }
+  if (a == 1) {
+    return 1;
+  }
+  return fib(a - 1) + fib(a - 2);
+}
+
+void test3() {
+  if (fib(10) == 55) {
+    printf("OK\n");
+  } else {
+    printf("NG\n");
+  }
+}
 
 int main() {
-  printf(s);
-  printf(t);
-  printf("!!!\n");
+  test1();
+  test2();
+  test3();
+
   int i;
   for (i = 0; i < 10; i = i + 1) {
     int j = i + 1;
     printf("1 + %d = %d\n", i, j);
     //printf("1 + %d = %d\n", i, i + 1);
   }
+
   return 0;
 }
 
